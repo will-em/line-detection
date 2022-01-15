78afc7c2f342a946bf6d4fcc8888027eab7a1eca
William Emanuelsson <wem@kth.se>
2022-01-15 19:19:01 +0100

    Working function call

--- src/Images.js
+++ src/Images.js
@@ -1,10 +1,12 @@
 import './App.css';
-import court from './Images/tennis_court.jpeg'
+import court from './Images/tennis_court.jpeg';
+import {print} from './Test';
 
 function Images() {
   return (
       <div className='Images'>
         <img src={court} />
+        <button onClick={print}> Test </button>
       </div>
   );
 }
