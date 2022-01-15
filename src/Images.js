d96b077d5b5706bbb706a32c5b5ddfffe29a7bc5
William Emanuelsson <wem@kth.se>
2022-01-15 19:15:48 +0100

    Display image

---
+++ src/Images.js
@@ -0,0 +1,12 @@
+import './App.css';
+import court from './Images/tennis_court.jpeg'
+
+function Images() {
+  return (
+      <div className='Images'>
+        <img src={court} />
+      </div>
+  );
+}
+
+export default Images;
