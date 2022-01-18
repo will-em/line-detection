c712ed121462ec06952f281be924d71125803b58
William Emanuelsson <wem@kth.se>
2022-01-19 00:20:34 +0100

    Add Convolution function

---
+++ src/Convolution.js
@@ -0,0 +1,36 @@
+// https://stackoverflow.com/questions/64669531/2d-convolution-for-javascript-arrays
+
+
+function uniform_array(len, value) {
+    let arr = new Array(len); for (let i=0; i<len; ++i) arr[i] = Array.isArray(value) ? [...value] : value;
+    return arr;
+}
+
+function conv_2d(kernel, array){
+    var result = uniform_array(array.length, uniform_array(array[0].length, 0));
+    var kRows = kernel.length;
+    var kCols = kernel[0].length;
+    var rows = array.length;
+    var cols = array[0].length;
+    // find center position of kernel (half of kernel size)
+    var kCenterX = Math.floor(kCols/2);
+    var kCenterY = Math.floor(kRows/2);
+    var i, j, m, n, ii, jj;
+
+    for(i=0; i < rows; ++i){          // for all rows
+        for(j=0; j < cols; ++j){          // for all columns
+            for(m=0; m < kRows; ++m){         // for all kernel rows
+                for(n=0; n < kCols; ++n){        // for all kernel columns
+                    // index of input signal, used for checking boundary
+                    ii = i + (m - kCenterY);
+                    jj = j + (n - kCenterX);
+                    // ignore input samples which are out of bound
+                    if(ii >= 0 && ii < rows && jj >= 0 && jj < cols){
+                        result[i][j] += array[ii][jj] * kernel[m][n];
+                    };
+                };
+            };
+        };
+    };
+    return result;
+};
