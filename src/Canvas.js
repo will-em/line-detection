65850584de05cd215ef1a94378fece2d3a1abc98
William Emanuelsson <wem@kth.se>
2022-01-20 01:54:31 +0100

    Add helper functions

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import {convolve2d} from './Convolution';
-import {image_to_grayscale, grayscale_arr_to_image} from './HelperFunctions'
+import {image_to_grayscale, grayscale_arr_to_image, array_to_mat, flatten, norm256} from './HelperFunctions'
 
 function Canvas() {
 
@@ -24,32 +24,24 @@ function Canvas() {
 
     useEffect(() => {
         const canvas = canvasRef.current;
-        console.log(image);
-        console.log(canvas);
+
         if(image && canvas){
             const ctx = canvas.getContext('2d');
             ctx.drawImage(image, 0, 0, canvas.width/2, canvas.height/2);
 
-            /* TEST convolve2d FUNCTION
-            const testArr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-            const testKernal = [[1, 2], [3, 4]];
-
-            console.log(convolve2d(testKernal, testArr))
-            */
-            /*
-            let grayscaleData = ctx.createImageData(canvas.width/2, canvas.height/2);
-            let imageData = ctx.getImageData(0, 0, canvas.width/2, canvas.height/2);
-            let data = imageData.data;
-            grayscaleData.data = grayscale_to_RGBA(RGBA_to_grayscale(data));
-            ctx.putImageData(grayscaleData, canvas.width/2, canvas.height/2)
-            */
-
             let imageData = ctx.getImageData(0, 0, canvas.width/2, canvas.height/2);
             let grayscaleArr = image_to_grayscale(imageData); 
-            console.log(imageData.data)
+
+            //grayscaleArr = flatten(norm256(array_to_mat(grayscaleArr, imageData.height, imageData.width)));
+
             grayscale_arr_to_image(grayscaleArr, imageData)
-            console.log(imageData.data)
             ctx.putImageData(imageData, canvas.width/2, canvas.height/2)
+
+            let mat = array_to_mat(grayscaleArr, imageData.height, imageData.width);
+            const kernel = [[1, 1, 1], [1, 2, 1], [1, 1, 1]];
+
+
+
        } 
     }, [image])
 
--- src/HelperFunctions.js
+++ src/HelperFunctions.js
@@ -1,3 +1,4 @@
+// Create grayscale array from image data (RGBA -> grayscale)
 export const image_to_grayscale = (ImageData) => { // Neglecting transparency 
     let grayArr = new Array(ImageData.width*ImageData.height);
     let j = 0;
@@ -9,6 +10,7 @@ export const image_to_grayscale = (ImageData) => { // Neglecting transparency
     return grayArr;
 }
 
+// Copy grayscale array onto image data
 export const grayscale_arr_to_image = (arr, ImageData) => {
     let j = 0;
     for(let i=0; i<arr.length; i++){
@@ -18,4 +20,53 @@ export const grayscale_arr_to_image = (arr, ImageData) => {
         ImageData.data[j + 3] = 255; // Full transparency 
         j+=4;
     }
+}
+
+
+// Reshape array to 2D-array
+export const array_to_mat = (arr, height, width) => {
+    let mat = [];
+    while(arr.length)
+        mat.push(arr.splice(0, width))
+    return mat;
+}
+
+// 2D-array to array
+export const flatten = (arr) => {
+    let height = arr.length;
+    let width = arr[0].length;
+
+    let flat = [];
+
+    for(let i=0; i<height; i++){
+        for(let j=0; j<width; j++){
+            flat[i*height + j] = arr[i, j];
+        }
+    }
+
+    return flat;
+}
+
+// Normalize 2D-array and multiply by 255
+export const norm256 = (arr) => {
+    let height = arr.length;
+    let width = arr[0].length;
+    let max = 0;
+    for(let i=0; i<height; i++){
+        for(let j=0; j<width; j++){
+            if(arr[i, j]>max)
+                max = arr[i, j];
+        }
+    }
+    if(max==255)
+        return arr;
+
+    let newMat = [...arr]; // Copy array
+    
+    for(let i=0; i<height; i++){
+        for(let j=0; j<width; j++){
+            newMat[i, j] = 255 * newMat[i, j] / max;
+        }
+    }
+
 }
