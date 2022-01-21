820e0b87dba8d5c2f91324eaa40044a64fcb104c
William Emanuelsson <wem@kth.se>
2022-01-21 01:22:47 +0100

    Working gaussian kernel

---
+++ src/GaussianBlur.js
@@ -0,0 +1,14 @@
+const gaussian = (x, y, t) => {
+    return 1/(2 * Math.PI * t) * Math.exp(-(x*x + y*y)/(2 * t));
+}
+
+export const gaussianKernel = (t, size) => {
+    const kernel = [...Array(size)].map(e => Array(size)); 
+    for(let i=0; i<size; i++){
+        for(let j=0; j<size; j++){
+            kernel[i][j] = gaussian(-size + i +1 , -size + j + 1, t);
+        }
+    }
+    console.log(kernel)
+    return kernel;
+}
