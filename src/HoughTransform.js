b05e6e0a2d9509f54456462cfbcb730848daba52
William Emanuelsson <wem@kth.se>
2022-01-27 02:56:09 +0100

    Work in progress accumulator generator

--- src/HoughTransform.js
+++ src/HoughTransform.js
@@ -8,3 +8,16 @@ const linspace = (start, end, N) => {
 
     return res;
 }
+
+export const get_accumulator = (arr, N_rho, N_theta) => {
+    let height = arr.length;
+    let width = arr[0].length;
+
+    let hyp = Math.round(Math.sqrt(height * height + width * width));
+    
+    let rho_arr = linspace(-hyp / 2, hyp / 2, N_rho);
+
+    let theta_arr = linspace(-Math.PI / 2, Math.PI, N_theta);
+
+    
+}
