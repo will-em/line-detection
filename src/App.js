660c4debcbb88791ea1282c861dd4441b4b1fb0f
William Emanuelsson <wem@kth.se>
2022-01-31 03:25:12 +0100

    Working sliders

--- src/App.js
+++ src/App.js
@@ -7,8 +7,15 @@ import Button from '@mui/material/Button';
 
 function App() {
   const [variance, setVariance] = useState(1);
+  const [low_t, setLow_t] = useState(0.05);
+  const [high_t, setHigh_t] = useState(0.5);
   const [image, setImage] = useState(null);
   const [generate, setGenerate] = useState(false);
+
+  useEffect(() => {
+    if(low_t > high_t)
+      setHigh_t(low_t);
+  },[low_t])
 
   const reader = new FileReader();
   const imageReader = e => {
@@ -25,7 +32,9 @@ function App() {
     <div>
       <Canvas variance={variance} uploadedImage={image} generate={generate} setGenerate={setGenerate}/>
       <Stack id="stack" spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
-        <VarSlider setVariance={setVariance}/>
+        <VarSlider value={variance} setValue={setVariance} min={0.05} max={5} step={0.05}/>
+        <VarSlider value={low_t} setValue={setLow_t} min={0.05} max={0.9} step={0.05}/>
+        <VarSlider value={high_t} setValue={setHigh_t} min={0.05} max={0.9} step={0.05} low_t={low_t}/>
         <Button variant="contained" onClick={() => setGenerate(true)}>Generate</Button>
         <Button variant="contained" component="label">Upload<input type="file" hidden onChange={imageReader}/></Button>
       </Stack>
--- src/VarSlider.js
+++ src/VarSlider.js
@@ -1,20 +1,26 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Slider from '@mui/material/Slider';
 
-function VarSlider({variance, setVariance}) {
+function VarSlider({min, max, step, value, setValue, low_t}) {
   
     const handleChange = (event, newValue) => {
-      setVariance(newValue);
+        // Make sure the higher threshold is larger than the lower one
+        if(low_t){
+            if(newValue >= low_t)
+                setValue(newValue);
+        }else{
+            setValue(newValue);
+        }
     };
   
     return (
         <Slider 
-        value={variance} 
+        value={value} 
         valueLabelDisplay="auto" 
         onChange={handleChange} 
-        min={0.05}
-        max={5}
-        step={0.05}
+        min={min}
+        max={max}
+        step={step}
         />
     );
   }
