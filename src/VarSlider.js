b74660c17920554f0bc88e84d69a24465bf23b4e
William Emanuelsson <wem@kth.se>
2022-02-06 23:34:07 +0100

    Remove unnecessary import

--- src/VarSlider.js
+++ src/VarSlider.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import Slider from '@mui/material/Slider';
 
 function VarSlider({min, max, step, value, setValue, low_t}) {
